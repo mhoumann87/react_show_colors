0ecfbad90bd308abb871ca61ae2f70b3f3ca0fc9
Michael Houmann <mhoumann87@gmail.com>
2023-04-15 13:19:43 +0200

    Add base content and style for Content component

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,4 +1,16 @@
 const Content = () => {
-  return <div>Content</div>;
+  return (
+    <main className='container'>
+      <div className='color-box box-shadow'>
+        <p class='clr-name'>Empty Value</p>
+      </div>
+      <input
+        className='box-shadow'
+        type='text'
+        placeholder='Add color name'
+        autofocus
+      />
+    </main>
+  );
 };
 export default Content;
